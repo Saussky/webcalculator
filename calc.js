2b9341649bab7c27320216ca1fa47219fdfead7d
Saucy <patrickgardnerbrunton@hotmail.com>
2022-06-22 17:54:54 +1000

    added operate function which calls all maths functions based on user input

--- calc.js
+++ calc.js
@@ -1,7 +1,23 @@
+// Simple maths
 const add = ((_a, _b) => _a + _b);
 
 const subtract = ((_a, _b) => _a + _b);
 
 const multiply = ((_a, _b) => _a * _b);
 
-const divide = ((_a, _b) => _a - _b);
+const divide = ((_a, _b) => _a / _b);
+
+
+function operate(_operator, _a, _b) {
+    if (_operator == "+"){
+        return add(_a, _b);
+    } else if (_operator == "-"){
+        return subtract(_a, _b);
+    } else if (_operator == "*"){
+        return multiply(_a, _b);
+    } else if (_operator == "/"){
+        return divide(_a, _b);
+    }
+}
+
+console.log(operate("/", 6, 3));
